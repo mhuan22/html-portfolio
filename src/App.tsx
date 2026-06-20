ff1a89986b4e3de402bc9c5f49e597c4cc04fc83
Mingwei Huang <124105125+mhuan22@users.noreply.github.com>
2026-06-20 14:02:11 -0600

    Create initial structure with react

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <main>
+      <h1>Mingwei Huang</h1>
+
+      <h2>Software Developer</h2>
+
+      <p>
+        Computer Science student interested in full-stack development and
+        building accessible software.
+      </p>
+    </main>
+  );
+}
+
+export default App;
